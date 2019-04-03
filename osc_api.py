48d548af98c01a92144d6682033b041c6fc5d589
Owen HT Lu <cfleu198@gmail.com>
2019-04-03 15:04:23 +0800

    Update osc_api.py

--- osc_api.py
+++ osc_api.py
@@ -260,7 +260,7 @@ def test_4():
     raw_list = raw_list[query_from[0]:query_to[-1]]
     
     print('query_from:', query_from[0])
-    print('query_to:', query_from[-1])
+    print('query_to:', query_to[-1])
     resp = osc_fft(raw_list)
 
     
